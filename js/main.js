1ace82edefd78be1311b8a94f6e1201f7e77dea3
Fauroro <151984072+Fauroro@users.noreply.github.com>
2024-02-28 07:47:14 -0500

    feat: :construction: Fix import json
    Fix import json

--- js/main.js
+++ js/main.js
@@ -1,254 +1,25 @@
-const marvel = [
-    {
-        "id": "5d86371f1efebc31def272e2",
-        "about": "Genio, multimillonario, playboy y filántropo. Tony Stark es un magnate de la industria tecnológica, creador de la poderosa armadura Iron Man. Su mente brillante y su corazón de héroe lo convierten en un líder destacado de los Avengers.",
-        "picture": "https://www.sideshow.com/storage/product-images/903421/iron-man_marvel_gallery_5c4cced10da7f.jpg",
-        "squarePic": "https://dam.smashmexico.com.mx/wp-content/uploads/2018/06/27181227/ironman_portada2.jpg",
-        "name": "Iron Man",
-        "fecha_creacion": "1963-03-01"
-    },
-    {
-        "id": "5d86371f2343e37870b91ef1",
-        "about": "Dr. Bruce Banner, un científico brillante, se transforma en Hulk, un monstruo verde de fuerza descomunal, cuando experimenta emociones intensas. Hulk es una fuerza imparable y un valioso miembro de los Avengers.",
-        "picture": "https://vignette.wikia.nocookie.net/marvelvscapcom/images/5/5d/HulkBruce.png/revision/latest?cb=20170818123736",
-        "squarePic": "https://bolavip.com/__export/1557252167649/sites/bolavip/img/2019/05/07/hulk_crop1557252167265.jpg_1693159006.jpg",
-        "name": "Hulk",
-        "fecha_creacion": "1962-05-01"
-    },
-    {
-        "id": "5d86371f25a058e5b1c8a65e",
-        "about": "Steve Rogers, el supersoldado de la Segunda Guerra Mundial, lidera a los Avengers con su escudo indestructible y su habilidad táctica. Su devoción por la justicia y su valentía lo convierten en un símbolo de esperanza.",
-        "picture": "https://i.pinimg.com/736x/ba/9b/36/ba9b3623c9a639296e85f7ff09c3c205.jpg",
-        "squarePic": "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2020/06/capitan-america-1969111.jpg?tf=3840x",
-        "name": "Captain America",
-        "fecha_creacion": "1941-03-01"
-    },
-    {
-        "id": "5d86371f9f80b591f499df32",
-        "about": "Carol Danvers, una piloto con poderes cósmicos, se convierte en Captain Marvel. Su fuerza sobrehumana y habilidades de vuelo la hacen una defensora poderosa del universo.",
-        "picture": "http://im.rediff.com/movies/2019/mar/06captain-marvel1.jpg",
-        "squarePic": "https://img.chilango.com/2018/05/quien-es-capitan-marvel.jpg",
-        "name": "Captain Marvel",
-        "fecha_creacion": "1968-03-01"
-    },
-    {
-        "id": "5d86371f233c9f2425f16916",
-        "about": "Natasha Romanoff, espía y agente de S.H.I.E.L.D., es conocida como Black Widow. Su destreza en combate y sus habilidades como espía la convierten en una fuerza formidable.",
-        "picture": "https://m.media-amazon.com/images/M/MV5BYTU4NmRhNTctMjI2ZS00ZTUyLWI1ZjEtZWU1ODAxMjM5MzlmXkEyXkFqcGdeQXVyNDQxNjcxNQ@@._V1_UY1200_CR165,0,630,1200_AL_.jpg",
-        "squarePic": "https://wipy.tv/wp-content/uploads/2019/07/filtran-el-tr%C3%A1iler-de-%E2%80%98Black-Widow%E2%80%99.jpg",
-        "name": "Black Widow",
-        "fecha_creacion": "1964-04-01"
-    },
-    {
-        "id": "5d86371f97c29d020f1e1f6d",
-        "about": "Sam Wilson, héroe de Marvel con alas mecánicas y aliado cercano de Captain America. Falcon utiliza sus habilidades de vuelo y combate para proteger a los inocentes y luchar contra las amenazas.",
-        "picture": "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/4/48/Falcon_AIW_Profile.jpg/revision/latest/scale-to-width-down/310?cb=20180518212822",
-        "squarePic": "https://www.hdwallpapers.in/download/anthony_mackie_falcon_marvel_comics_sam_wilson_with_wings_hd_4k_the_falcon_and_the_winter_soldier-HD.jpg",
-        "name": "Falcon",
-        "fecha_creacion": "1969-09-01"
-    },
-    {
-        "id": "5d86371fd55e2e2a30fe1ccb",
-        "about": "T'Challa, rey de Wakanda, es el Pantera Negra. Con su traje de vibranium y habilidades mejoradas, protege su nación y lucha por la justicia como miembro de los Avengers.",
-        "picture": "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/9/99/Black_Panther_AIW_Profile.jpg/revision/latest/scale-to-width-down/310?cb=20180518212436",
-        "squarePic": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C4E26547ADB216527A0A75F01774F40189FFFB18ACE28727F3567072FE1C6942/scale?width=1200&aspectRatio=1.78&format=webp",
-        "name": "Black Panther",
-        "fecha_creacion": "1966-07-01"
-    },
-    {
-        "id": "5d86371fd55e2e2a30fe1ccb1",
-        "about": "Stephen Strange es un maestro de las artes místicas. Después de una lesión, se convierte en Doctor Strange, utilizando la magia para defender la realidad de amenazas sobrenaturales.",
-        "picture": "https://www.outland.no/media/catalog/product/cache/6f3b753be090e58846a92333dfe1de97/4/8/4897011186276_4.jpg",
-        "squarePic": "https://www.revistafactum.com/wp-content/uploads/2016/11/ds0.jpg",
-        "name": "Doctor Strange",
-        "fecha_creacion": "1963-07-01"
-    },
-    {
-        "id": "5d86371fd55e2e2a30fe1ccb2",
-        "about": "Peter Parker, el amigable vecino Spider-Man, posee habilidades arácnidas. Con su agilidad y sentido arácnido, Spider-Man combate el crimen en Nueva York.",
-        "picture": "https://www.sideshow.com/storage/product-images/903735/spider-man-advanced-suit_marvel_silo.png",
-        "squarePic": "https://e00-marca.uecdn.es/assets/multimedia/imagenes/2019/09/28/15696592030335.jpg",
-        "name": "Spider Man",
-        "fecha_creacion": "1962-08-01"
-    },
-    {
-        "id": "5d86371fd55e2e2a30fe1cc3",
-        "about": "Scott Lang, un ladrón convertido en héroe, puede reducir su tamaño con tecnología especial. Ant-Man utiliza sus habilidades únicas para la infiltración y el combate.",
-        "picture": "https://fsmedia.imgix.net/f5/ba/d8/66/7dd4/44e9/8e0a/d91348489582/ant-man-and-the-wasp-2018.jpeg#image",
-        "squarePic": "https://www.mundodeportivo.com/alfabeta/hero/2020/12/Ant-Man-1.jpg?width=1200",
-        "name": "Ant Man",
-        "fecha_creacion": "1962-11-01"
-    },
-    {
-        "id": "5d86371fd55e2e2a30fe1cc4",
-        "about": "Heroína conocida como La Avispa, puede cambiar de tamaño y volar. Es una valiosa miembro de los Avengers, aportando versatilidad y poder en el campo de batalla.",
-        "picture": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Qum9HPm82yeUaFuMhpo3A98HbgsyUSDmk1cUNS4C2rb31BHZ2w",
-        "squarePic": "https://www.sideshow.com/cdn-cgi/image/quality=90,f=auto/https://www.sideshow.com/storage/product-images/912131/the-wasp_marvel_gallery_63e153ae51302.jpg",
-        "name": "The Wasp",
-        "fecha_creacion": "1963-06-01"
-    },
-    {
-        "id": "5d86371fd55es52a30fe1cc4",
-        "about": "Astuto mapache genéticamente modificado y miembro de los Guardianes de la Galaxia. Rocket es un experto en tecnología y tácticas de combate.",
-        "picture": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Qum9HPm82yeUaFuMhpo3A98HbgsyUSDmk1cUNS4C2rb31BHZ2w",
-        "squarePic": "https://www.sideshow.com/storage/product-images/902965/rocket-deluxe-version_marvel_feature.jpg",
-        "name": "Rocket",
-        "fecha_creacion": "1976-01-01"
-    },
-    {
-        "id": "5d86fdasd4552a30fe1cc4",
-        "about": "Bruja escarlata con poderes de manipulación de la realidad. Wanda Maximoff es una fuerza poderosa, pero a veces incontrolable, en el universo Marvel.",
-        "picture": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Qum9HPm82yeUaFuMhpo3A98HbgsyUSDmk1cUNS4C2rb31BHZ2w",
-        "squarePic": "https://www.murphysmultiverse.com/wp-content/uploads/2022/12/scarletwitchdead.jpg",
-        "name": "Scarlet Witch",
-        "fecha_creacion": "1964-03-01"
-    },
-    {
-        "id": "5d86371fes52a30fsfe54c4",
-        "about": "Miembro fuerte de los Guardianes de la Galaxia con sed de venganza. Drax, el Destructor, busca vengar la muerte de su familia.",
-        "picture": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Qum9HPm82yeUaFuMhpo3A98HbgsyUSDmk1cUNS4C2rb31BHZ2w",
-        "squarePic": "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2021/08/guardianes-galaxia-drax-destructor-2427449.jpg?tf=3840x",
-        "name": "Drax",
-        "fecha_creacion": "1973-05-01"
-    },
-    {
-        "id": "5d863asf5asdf52a30fe1cc4",
-        "about": "Astuto dios nórdico, hermano de Thor, con habilidades mágicas y una historia compleja. Loki, a veces villano y a veces aliado, es un personaje intrigante con motivaciones cambiantes.",
-        "picture": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Qum9HPm82yeUaFuMhpo3A98HbgsyUSDmk1cUNS4C2rb31BHZ2w",
-        "squarePic": "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2021/05/loki-tom-hiddleston-2345851.jpg?tf=3840x",
-        "name": "Loki",
-        "fecha_creacion": "1962-08-01"
-    }
-];
+let marvel = [];
+let dc = [];
+let data = [];
+document.addEventListener('DOMContentLoaded',async (e) =>{
+    data = await loadData2();
+    marvel = data[0];
+    dc = data[1];
+    crearCard(marvel,cardMarvel,'marvel');
+    crearCard(dc,cardDc,'dc');
+});
 
-const dc = [
-    {
-        "id": "1",
-        "name": "Superman",
-        "about": "Kal-El, el último hijo de Krypton, es un alienígena con poderes sobrehumanos en la Tierra. Con fuerza, velocidad y visión de calor, Superman defiende la justicia y la verdad.",
-        "picture": "superman_vertical.jpg",
-        "squarePic": "superman_horizontal.jpg",
-        "fecha_creacion": "1938-04-18"
-    },
-    {
-        "id": "2",
-        "name": "Batman",
-        "about": "Bruce Wayne, un millonario traumatizado por el asesinato de sus padres, lucha contra el crimen en Gotham como Batman. Con habilidades detectivescas y tecnología avanzada, es un justiciero en la oscuridad.",
-        "picture": "batman_vertical.jpg",
-        "squarePic": "batman_horizontal.jpg",
-        "fecha_creacion": "1939-03-30"
-    },
-    {
-        "id": "3",
-        "name": "Wonder Woman",
-        "about": "Diana Prince, princesa amazona y semidiosa, posee habilidades divinas y porta la Lazo de la Verdad. Como embajadora de Themyscira, Wonder Woman lucha por la paz y la justicia.",
-        "picture": "wonder_woman_vertical.jpg",
-        "squarePic": "wonder_woman_horizontal.jpg",
-        "fecha_creacion": "1941-10-21"
-    },
-    {
-        "id": "4",
-        "name": "The Flash",
-        "about": "Barry Allen, afectado por un rayo y productos químicos, obtiene la capacidad de moverse a velocidades sobrehumanas. Como The Flash, protege Central City y es miembro clave de la Liga de la Justicia.",
-        "picture": "the_flash_vertical.jpg",
-        "squarePic": "the_flash_horizontal.jpg",
-        "fecha_creacion": "1940-01-06"
-    },
-    {
-        "id": "5",
-        "name": "Aquaman",
-        "about": "Arthur Curry, mitad atlante y mitad humano, es el rey de Atlantis con la capacidad de comunicarse con criaturas marinas. Aquaman defiende los océanos y su pueblo con valentía.",
-        "picture": "aquaman_vertical.jpg",
-        "squarePic": "aquaman_horizontal.jpg",
-        "fecha_creacion": "1941-11-01"
-    },
-    {
-        "id": "6",
-        "name": "Green Lantern",
-        "about": "Portador del anillo de poder que confiere habilidades sobrenaturales, Green Lantern es el protector del sector espacial 2814. Diversos individuos han llevado el anillo, cada uno con su propio juramento.",
-        "picture": "green_lantern_vertical.jpg",
-        "squarePic": "green_lantern_horizontal.jpg",
-        "fecha_creacion": "1940-07-01"
-    },
-    {
-        "id": "7",
-        "name": "Cyborg",
-        "about": "Victor Stone, atleta y estudiante convertido en héroe, tiene partes del cuerpo cibernéticas después de un accidente. Cyborg es un miembro clave de la Liga de la Justicia con habilidades tecnológicas impresionantes.",
-        "picture": "cyborg_vertical.jpg",
-        "squarePic": "cyborg_horizontal.jpg",
-        "fecha_creacion": "1980-07-01"
-    },
-    {
-        "id": "8",
-        "name": "Shazam",
-        "about": "Billy Batson, un niño transformado en un superhéroe adulto al pronunciar la palabra 'Shazam'. Con poderes mágicos otorgados por figuras mitológicas, Shazam es un defensor de la verdad y la justicia.",
-        "picture": "shazam_vertical.jpg",
-        "squarePic": "shazam_horizontal.jpg",
-        "fecha_creacion": "1939-02-01"
-    },
-    {
-        "id": "9",
-        "name": "Green Arrow",
-        "about": "Oliver Queen, arquero y justiciero, combate el crimen en Star City. Green Arrow es conocido por su habilidad con el arco y la flecha, así como por su sentido de la justicia.",
-        "picture": "green_arrow_vertical.jpg",
-        "squarePic": "green_arrow_horizontal.jpg",
-        "fecha_creacion": "1941-11-01"
-    },
-    {
-        "id": "10",
-        "name": "Batwoman",
-        "about": "Kate Kane, prima de Batman, lucha contra el crimen en Gotham como Batwoman. Con habilidades de combate excepcionales y tecnología avanzada, es una defensora intrépida de la justicia.",
-        "picture": "batwoman_vertical.jpg",
-        "squarePic": "batwoman_horizontal.jpg",
-        "fecha_creacion": "1956-03-01"
-    },
-    {
-        "id": "11",
-        "name": "Martian Manhunter",
-        "about": "J'onn J'onzz, el último marciano verde, posee habilidades sobrehumanas. Como miembro de la Liga de la Justicia, Martian Manhunter protege la Tierra contra amenazas cósmicas.",
-        "picture": "martian_manhunter_vertical.jpg",
-        "squarePic": "martian_manhunter_horizontal.jpg",
-        "fecha_creacion": "1955-11-01"
-    },
-    {
-        "id": "12",
-        "name": "Catwoman",
-        "about": "Selina Kyle, ladrona y antiheroína, a veces aliada y a veces enemiga de Batman. Catwoman es conocida por su destreza atlética y su habilidad para sortear la ley.",
-        "picture": "catwoman_vertical.jpg",
-        "squarePic": "catwoman_horizontal.jpg",
-        "fecha_creacion": "1940-04-01"
-    },
-    {
-        "id": "13",
-        "name": "Harley Quinn",
-        "about": "Harleen Quinzel, antigua psiquiatra, se convierte en la caótica compañera del Joker. Harley Quinn es conocida por su personalidad excéntrica y su relación tumultuosa con el Príncipe Payaso del Crimen.",
-        "picture": "harley_quinn_vertical.jpg",
-        "squarePic": "harley_quinn_horizontal.jpg",
-        "fecha_creacion": "1992-09-01"
-    },
-    {
-        "id": "14",
-        "name": "The Atom",
-        "about": "Ray Palmer, científico capaz de cambiar su tamaño hasta niveles subatómicos. The Atom utiliza su habilidad para la miniaturización en la lucha contra el crimen y la exploración científica.",
-        "picture": "the_atom_vertical.jpg",
-        "squarePic": "the_atom_horizontal.jpg",
-        "fecha_creacion": "1961-10-01"
-    },
-    {
-        "id": "15",
-        "name": "Zatanna",
-        "about": "Zatanna Zatara, maga y hechicera con habilidades místicas. Con la capacidad de lanzar hechizos invocando palabras al revés, Zatanna es una poderosa defensora del equilibrio mágico.",
-        "picture": "zatanna_vertical.jpg",
-        "squarePic": "zatanna_horizontal.jpg",
-        "fecha_creacion": "1964-11-01"
-    }
-];
+async function loadData2(){
+    let dataMarvel = await fetch('../json/dataMarvel.json').then(response => response.json()); 
+    let dataDC = await fetch('../json/dataDC.json').then(response => response.json()); 
+    return [dataMarvel,dataDC];
+}
 
 const cardMarvel = document.querySelector('.cardMarvel');
 const cardDc = document.querySelector('.cardDc');
+const sectionMarvel = document.getElementById('marvel');
 
-crearCard(marvel,cardMarvel,'marvel');
-crearCard(dc,cardDc,'dc');
+// sectionMarvel.style.display = 'none'
 
 function crearCard(arreglo,padre,franquicia){
     arreglo.forEach(item => {
@@ -310,16 +81,17 @@ function buscar(id,franquicia){
 }
 
 function mostrarModal(hero){
-    dialog.show()
     imgModal.src = hero.picture;
     tituloModal.textContent = hero.name;
     descripcion.textContent = hero.about;
     fecha.innerHTML = `<strong>Fecha de creacion: </strong> ${hero.fecha_creacion}`
     opacity.style.display = 'block';
+    dialog.show();
 }
 
 btnCerrar.addEventListener('click', function (){
     dialog.close()
+    imgModal.src = "";
     opacity.style.display = 'none';
 });
 
